c598a06a57a6bc07e9af6d9c0c382c1306b44ee6
Tohidur Islam Mondal <tohidurim@gmail.com>
2023-04-02 13:53:48 +0530

    fifth commit

---
+++ Reverse.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class Reverse {
+
+    public static void main(String[] args) {
+
+// Read input string from user
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the input string: ");
+        String input = scanner.nextLine();
+
+        // Split the input string into words
+        String[] words = input.split("(?<=\\D)(?=\\d)|(?<=\\d)(?=\\D)");
+
+        // Reverse the order of the words
+        int left = 0, right = words.length - 1;
+        while (left < right) {
+
+            // checking if the left string is numeric
+            boolean isDigit = Character.isDigit(words[left].charAt(0));
+            if (isDigit) {
+                left++;
+            }
+
+            // checking if the right string is numeric
+            boolean isDigit2 = Character.isDigit(words[right].charAt(0));
+            if (isDigit2) {
+                right--;
+            }
+            if (left < right) {
+                String temp = words[left];
+                words[left] = words[right];
+                words[right] = temp;
+                left++;
+                right--;
+            }
+
+        }
+
+        // Join the words back together into a single string
+        String output = String.join("", words);
+
+        // Print the final output
+        System.out.println("Output: " + output);
+    }
+
+}
